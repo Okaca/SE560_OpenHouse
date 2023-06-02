4afdad84a78afb39fbf35a66e72d0b84f84d0cee
Onur Kağan Coşkun <onurkagancoskun@gmail.com>
2023-06-02 14:01:19 +0300

    restore

---
+++ app/trips/page.tsx
@@ -0,0 +1,47 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+import TripsClient from "./TripsClient";
+
+const TripsPage = async () => {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+        return (
+            <ClientOnly>
+                <EmptyState 
+                    title="Yetkisiz İşlem"
+                    subtitle="Lütfen Giriş Yapın"
+                />
+            </ClientOnly> 
+        )
+    }
+
+    const reservations = await getReservations({
+        userId: currentUser.id
+    });
+
+    if (reservations.length === 0) {
+        return (
+            <ClientOnly>
+                <EmptyState 
+                    title="Rezervasyon bulunamadı"
+                    subtitle="Henüz bir rezervasyonun yok"
+                />
+            </ClientOnly>
+        )
+    }
+
+    return (
+        <ClientOnly>
+            <TripsClient 
+                reservations={reservations}
+                currentUser={currentUser}
+            />
+        </ClientOnly>
+    )
+}
+
+export default TripsPage;
